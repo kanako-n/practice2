4e5484edc7fadda42f4a56436499042e5782b9e7
Kanako Nagiri <kanako22n@gmail.com>
2020-04-09 15:43:25 +0900

    swiper部分作成

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// results
+new Swiper( '.swiper-container', {
+  speed: 400,
+  spaceBetween: 40,
+  width: 422,
+  loop: true,
+  loopedSlides: 6,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+  // breakpoints: {
+  //   768: {
+  //     slidesPerView: 1.5,
+  //     spaceBetween: 24,
+  //   }
+  // }
+});
