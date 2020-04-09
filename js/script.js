4de7bab5bb112dcf6dfe6758227c1ab38e32c70e
Kanako Nagiri <kanako22n@gmail.com>
2020-04-09 22:43:21 +0900

    faq途中まで

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-// results
+/*resultsスワイパー
+  -------------------------------------------------*/
 new Swiper( '.swiper-container', {
   speed: 400,
   spaceBetween: 40,
@@ -16,4 +17,28 @@ new Swiper( '.swiper-container', {
   //     spaceBetween: 24,
   //   }
   // }
+});
+
+/* FAQのアコーディオン
+  ------------------------------------------------*/
+//クリックしたときに答えの表示中はopenをつけ、非表示中は外す
+$('.faq-list__item').click(function() {
+  var $answer = $(this).find('.answer');
+  if($answer.hasClass('open')) { 
+    $answer.removeClass('open');
+    // slideUpメソッドを用いて、$answerを隠す
+    $answer.slideUp();
+
+    // 子要素のspanタグの中身をtextメソッドを用いて書き換える
+    $(this).find('span').text('+');
+    
+  } else {
+    $answer.addClass('open'); 
+    // slideDownメソッドを用いて、$answerを表示
+    $answer.slideDown();
+    
+    // 子要素のspanタグの中身をtextメソッドを用いて書き換える
+    $(this).find('span').text('-');
+    
+  }
 });
