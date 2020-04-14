f068da4d3b84bf0989756d273c795deda562e79e
Kanako Nagiri <kanako22n@gmail.com>
2020-04-14 20:37:38 +0900

    news top修正

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+/*スムーススクロール
+  ------------------------------------------------*/
+  $(function(){
+    $('a[href^="#"]').click(function(){
+      var speed = 500;
+      var href= $(this).attr("href");
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      var position = target.offset().top;
+      $("html, body").animate({scrollTop:position}, speed, "swing");
+      return false;
+    });
+  });
+
+
 /*resultsスワイパー
   -------------------------------------------------*/
   if (window.matchMedia( "(max-width: 768px)" ).matches) {
